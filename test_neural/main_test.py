b867c88cd89b1e98304a8656f2eb505eaf043e8a
krabby1611 <danilmelnikov11@gmail.com>
2020-07-30 17:15:03 +0500

    test change

--- test_neural/main_test.py
+++ test_neural/main_test.py
@@ -20,6 +20,5 @@ w1 = np.array(([0.45,-0.12,0.78,0.13]),float).reshape([2,2])
 w2 = np.array(([1.5,-2.3]),float).reshape([1,2])
 
 input_data2 = f(w1,input_data)
-print(input_data2)
 res = f(w2,input_data2)
 print(res)
